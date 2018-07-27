75c12f65f329db5b03c50d3f4f967d86c6ee8231
enes ozbek <enes_ozbek@hotmail.com>
2018-07-27 15:34:27 +0300

    problem for - comma seperator is solved.

--- Formatter/Formatter.cs
+++ Formatter/Formatter.cs
@@ -12,11 +12,16 @@ namespace NordeaFormatter
         public List<Sentence> Format(string input)
         {
             List<Sentence> formattedSentences = new List<Sentence>();
-            var sentences = input.Split(_sentenceDelimiters, StringSplitOptions.RemoveEmptyEntries);
+            var sentences = input.Split(_sentenceDelimiters, StringSplitOptions.RemoveEmptyEntries).Select(p=> p.Replace(",",", "));
+
+            var xx = sentences.Select(p => p.Replace(",", ", "));
 
             foreach (var item in sentences)
             {
                 var punctuation = item.Where(Char.IsPunctuation).Distinct().ToArray();
+
+                //var xx = item.Split().Select(p => p. (punctuation));
+                var kk = item.Split(punctuation).OrderBy(p=>p).ToList();
                 var words = item.Split().Select(x => x.Trim(punctuation)).Where(p=>!string.IsNullOrWhiteSpace(p)).OrderBy(p => p).ToList();
 
                 if (words.Any())
